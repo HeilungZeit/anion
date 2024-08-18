1e13d22452206ce443913809ac1ed175b7a0bc61
Dmitry Gavrichenko <kamael11319@gmail.com>
2024-08-18 22:25:46 +0200

    small fix

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,24 +1,13 @@
-import { Component, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { TUI_SANITIZER, TuiSvgModule, TuiSvgService } from '@taiga-ui/core';
-import { tuiIconApertureLarge } from '@taiga-ui/icons';
-import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
+import { TuiIcons } from '@taiga-ui/core';
 
 @Component({
   selector: 'ani-header',
   standalone: true,
-  imports: [TuiSvgModule, RouterLink, RouterLinkActive],
-  providers: [
-    {
-      provide: TUI_SANITIZER,
-      useClass: NgDompurifySanitizer,
-    },
-  ],
+  imports: [TuiIcons, RouterLink, RouterLinkActive],
+
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderLayout {
-  constructor(@Inject(TuiSvgService) tuiSvgService: TuiSvgService) {
-    tuiSvgService.define({ tuiIconApertureLarge });
-  }
-}
+export class HeaderLayout {}
