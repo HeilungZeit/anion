750d51c691bc7afb3b32c95905474ac8a3d998d5
Dmitry Gavrichenko <kamael11319@gmail.com>
2024-08-18 22:03:31 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,51 @@
-import { Component } from '@angular/core';
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
+import {
+  TuiRootModule,
+  TuiDialogModule,
+  TuiAlertModule,
+  TUI_SANITIZER,
+  TuiButtonModule,
+  TuiThemeNightModule,
+  TuiModeModule,
+  TuiNightThemeService,
+} from '@taiga-ui/core';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { ShikimoriService } from './services/shikimori/shikimori.service';
+import { AnimesStore } from './store/animes.store';
+import { HeaderLayout } from './layouts/header/header.component';
+import { FooterLayout } from './layouts/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    TuiButtonModule,
+    TuiThemeNightModule,
+    TuiModeModule,
+    TuiThemeNightModule,
+    TuiModeModule,
+    HeaderLayout,
+    FooterLayout,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  providers: [
+    { provide: TUI_SANITIZER, useClass: NgDompurifySanitizer },
+    ShikimoriService,
+    AnimesStore,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'anion';
+  constructor(
+    @Inject(TuiNightThemeService) readonly night$: Observable<boolean>
+  ) {}
 }
